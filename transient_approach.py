5c913112de3bff8ee23c285fe927598cae2b9eb1
Danny Eckerberg <deckerberg1@gmail.com>
2025-10-10 11:15:07 -0500

    updated transient_approach.py with T-varying h and cp

--- transient_approach.py
+++ transient_approach.py
@@ -3,10 +3,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
+from calculateh import calculate_h, cp_J
 
-# can dimensions (cm)
-D = 6
-h = 12
+# can dimensions (mm)
+D = 0.06
+h = 0.12
 Asurf = 2*np.pi*(D/2)*h + 2*np.pi*(D/2)**2
 V = np.pi*(D/2)**2*h
 
@@ -16,30 +17,22 @@ T_oo = 273.15
 T_target = 280.372
 
 # material properties
-rho_water = 1 # g/cc
-cp = 4200  #ph, J/kg/K
-h = 1000   #ph, W/m2/K
-
-# lumped parameters
-# def C(T):
-#     return rho_water*cp(T)*V
-
-# def U(T):
-#     return 6
-
+rho_water = 1000 # kg/m3
 
 # transient lumped parameter equation
-def T_prime(t, T_bar, T_oo, cp, h):
+def T_prime(t, T_bar, T_oo):
+    cp = cp_J(T_bar)
+    h = calculate_h(T_bar, 'natural')
     return -(h*Asurf / (rho_water*cp*V))*(T_bar-T_oo)
 
 # times of interest
 t_initial = 0
-t_final = 20
-t_eval = np.linspace(t_initial,t_final, 200)
+t_final = 1000
+t_eval = np.linspace(t_initial,t_final, 1000)
 
 
 sol = solve_ivp(T_prime, [t_initial, t_final], [T_hotcar],
-                args = (T_oo, cp, h),
+                args = (T_oo,),
                 t_eval=t_eval)
 
 plt.plot(sol.t, sol.y[0], "k")
